c38cd4cb834960316fed9846b01290dbb70ae255
Juan5757 <52605634+Juan5757@users.noreply.github.com>
2020-02-25 15:38:56 -0400

    Refactoring - método setNombre()

---
+++ ProductoAVenta/src/Servicio.java
@@ -0,0 +1,16 @@
+
+public class Servicio {
+
+	public String nombre;
+
+	public String getNombre() {
+		// TODO Auto-generated method stub
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		// TODO Auto-generated method stub
+		this.nombre=nombre;
+	}
+
+}
